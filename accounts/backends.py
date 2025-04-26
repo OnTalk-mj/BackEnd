28c9ed32df3778e413e468a4601542991e7c02d4
mlngwan <david027301@naver.com>
2025-04-27 03:30:16 +0900

    :memo: class명 변경

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.backends import ModelBackend
 from accounts.models import User
 
-class Email(ModelBackend):
+class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         email = kwargs.get('email') or username
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
             return None
-        if user.check_password(password):
+
+        if user.check_password(password) and self.user_can_authenticate(user):
             return user
         return None
