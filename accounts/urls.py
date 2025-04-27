fbb760592d6b80517409a9b573b65a86a77026de
김주한 <rlawngks07@gmail.com>
2025-04-28 00:51:49 +0900

    feat: 중복확인 이메일로그인 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SignupView, LoginView, IDCheckView, UserInfoView, UserUpdateView
+from .views import SignupView, LoginView, EmailCheckView, UserInfoView, UserUpdateView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
-    path('id-check/', IDCheckView.as_view(), name='id-check'),\
+    path('email-check/', EmailCheckView.as_view(), name='email-check'),
     path('mypage/', UserInfoView.as_view(), name='userinfo'),
     path('mypage/update/', UserUpdateView.as_view(), name='userinfo-update'),
 ]
