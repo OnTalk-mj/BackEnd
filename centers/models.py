c2f6d746273874db14b5af2a2a25647a09ad1091
mlngwan <david027301@naver.com>
2025-05-12 04:47:04 +0900

    :art: 상담센터 데이터 베이스 삽입을 위한 model 수정

--- centers/models.py
+++ centers/models.py
@@ -6,8 +6,9 @@ class CounselingCenter(models.Model):
     category = models.CharField(max_length=100)
     address = models.TextField()
     phone = models.CharField(max_length=20, blank=True)
+    website = models.URLField(max_length=255, null=True, blank=True)
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.region})"
