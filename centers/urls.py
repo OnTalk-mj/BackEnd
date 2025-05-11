800a6edb800aade1941729b43b51125cbda1f6ba
mlngwan <david027301@naver.com>
2025-05-12 04:47:41 +0900

    :art: 상담센터 데이터 베이스 삽입을 위한 url 수정

--- centers/urls.py
+++ centers/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import CounselingCenterListView
 
 urlpatterns = [
-    path('', CounselingCenterListView.as_view(), name='center-list'),
+    path('centerlist/', CounselingCenterListView.as_view(), name='center-list'),
 ]
